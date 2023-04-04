50f0b7664fe9975b9720637bf2beddb757fa824c
Sbani3010 <firebasedev20@gmail.com>
2023-04-04 21:10:12 +0200

    Snack entity

---
+++ src/main/java/za/ac/cput/domain/Snack.java
@@ -0,0 +1,51 @@
+/* ShowTime.java
+ Entity for the ShowTime
+ Author: Sbani Sithole (219446563)
+*/
+package za.ac.cput.domain;
+public class Snack {
+    private String id;
+    private String name;
+    private String price;
+
+    private Snack() {
+    }
+
+    private Snack(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.price = builder.price;
+    }
+
+    public static class Builder {
+        private String id;
+        private String name;
+        private String price;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setPrice(String price) {
+            this.price = price;
+            return this;
+        }
+
+        public Builder copy(Snack snack) {
+            this.id = snack.id;
+            this.name = snack.name;
+            this.price = snack.price;
+            return this;
+        }
+
+        public Snack build() {
+            return new Snack(this);
+        }
+    }
+}
