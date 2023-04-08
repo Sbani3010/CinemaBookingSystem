52a001392099b3cc8b82b6714c07deda46c7d3fb
Sbani3010 <firebasedev20@gmail.com>
2023-04-09 01:59:06 +0200

    project updated

--- src/main/java/za/ac/cput/factory/SnackFactory.java
+++ src/main/java/za/ac/cput/factory/SnackFactory.java
@@ -11,7 +11,7 @@ import za.ac.cput.domain.Snack;
 import za.ac.cput.util.Helper;
 
 public class SnackFactory {
-        public static Snack createSnack(String price, String name){
+        public static Snack createSnack(double price, String name){
             if(Helper.isNullOrEmpty(price) || Helper.isNullOrEmpty(name))
                 return null;
             return new Snack.Builder().setPrice(price)
