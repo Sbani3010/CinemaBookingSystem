fe046e7d46723bfe155362ad2498c8990b07420f
Sbani3010 <firebasedev20@gmail.com>
2023-04-09 16:47:49 +0200

    tests

---
+++ src/test/java/za/ac/cput/factory/ProofOfPaymentFactoryTest.java
@@ -0,0 +1,18 @@
+/* MovieRepository.java
+ Entity for the Movie
+ Author: Sbani Sithole (219446563)
+*/
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.util.TestObjects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProofOfPaymentFactoryTest {
+    @Test
+    void test(){
+        assertNotNull(TestObjects.payment);
+    }
+
+}
