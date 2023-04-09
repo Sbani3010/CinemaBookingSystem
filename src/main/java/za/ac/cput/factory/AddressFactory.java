e08748ecb6e34bd7e0f6132a43e2676d4d2c11c7
Sinethemba <www.sthelasne@gmail.com>
2023-04-09 12:44:05 +0100

    Tests for factory and repository

--- src/main/java/za/ac/cput/factory/AddressFactory.java
+++ src/main/java/za/ac/cput/factory/AddressFactory.java
@@ -1,3 +1,9 @@
+/*
+ * AddressFactory.java
+ * Factory for Address
+ * Author: Sinethemba Sithela 219112940
+ * Date: 04/04/2023
+ */
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Address;
--- src/main/java/za/ac/cput/repository/AddressRepository.java
+++ src/main/java/za/ac/cput/repository/AddressRepository.java
@@ -1,3 +1,9 @@
+/*
+ * AddressRepository.java
+ * Repository for Address
+ * Author: Sinethemba Sithela 219112940
+ * Date: 04/04/2023
+ */
 package za.ac.cput.repository;
 
 import za.ac.cput.domain.Address;
