775294813fe60053e064f73547ec80c330cfcdf7
Luphiwe <sluphiwe@gmail.com>
2023-06-12 00:14:05 +0200

    Service Implementation

---
+++ src/main/java/za/ac/cput/service/impl/RatingServiceImpl.java
@@ -0,0 +1,55 @@
+package za.ac.cput.service.impl;
+
+import za.ac.cput.domain.Rating;
+import za.ac.cput.repository.RatingRepository;
+import za.ac.cput.service.RatingService;
+
+import java.util.Set;
+
+public class RatingServiceImpl implements RatingService {
+
+    private static RatingServiceImpl service = null;
+    private RatingRepository repository=null;
+
+    private RatingServiceImpl()  {
+        if (repository == null) {
+            repository =  RatingRepositoryImpl.getRepository();
+        }
+
+    }
+    public static RatingServiceImpl getService() {
+        if (service == null) {
+            service = new RatingServiceImpl();
+        }
+        return service;
+    }
+
+    @Override
+    public Rating create(Rating rating) {
+        Rating created = repository.create(rating);
+        return created;
+    }
+
+    public Rating read(String id){
+        Rating readRating= repository.read(id);
+        return readRating;
+    }
+
+    public Rating update(Rating rating){
+        Rating updated = repository.update(rating);
+        return updated;
+    }
+
+    @Override
+    public boolean delete(String id)
+    {
+        boolean success = repository.delete(id);
+        return success;
+    }
+
+    @Override
+    public Set<Rating> getAll() {
+        return repository.getAll();
+    }
+}
+
---
+++ src/main/java/za/ac/cput/service/impl/TicketServiceImpl.java
@@ -0,0 +1,58 @@
+package za.ac.cput.service.impl;
+
+import za.ac.cput.domain.Rating;
+import za.ac.cput.domain.Ticket;
+import za.ac.cput.repository.RatingRepository;
+import za.ac.cput.repository.TicketRepository;
+import za.ac.cput.service.TicketService;
+
+import java.util.Set;
+
+public class TicketServiceImpl implements TicketService {
+
+    private static TicketServiceImpl service = null;
+    private TicketRepository repository=null;
+
+    private TicketServiceImpl()  {
+        if (repository == null) {
+            repository =  TicketRepositoryImpl.getRepository();
+        }
+
+    }
+    public static RatingServiceImpl getService() {
+        if (service == null) {
+            service = new TicketServiceImpl();
+        }
+        return service;
+    }
+
+
+    @Override
+    public Ticket create(Ticket ticket) {
+        Ticket created = repository.create(ticket);
+        return created;
+    }
+
+    @Override
+    public Ticket read(String id) {
+        Ticket readTicket= repository.read(id);
+        return readTicket;
+    }
+
+    @Override
+    public Ticket update(Ticket ticket) {
+        Ticket updated = repository.update(ticket);
+        return updated;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        boolean success = repository.delete(id);
+        return success;
+    }
+
+    @Override
+    public Set<Ticket> getAll() {
+        return repository.getAll();
+    }
+}
