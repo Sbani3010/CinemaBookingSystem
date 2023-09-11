f29c78171d029f0b259f6b4bee2cec22220eebb6
Sbani3010 <firebasedev20@gmail.com>
2023-09-11 10:24:27 +0200

    added auto generated to users

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -8,11 +8,14 @@
 package za.ac.cput.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
     private String name;
     private String surname;
